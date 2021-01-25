31af9b070050a02cce780ec0cf9e66a79acc7501
Seth Bashford <seth.bashford@boystown.org>
2021-01-25 14:01:35 -0600

    add test

--- spec/TokenController.js
+++ spec/TokenController.js
@@ -1,9 +1,17 @@
 import { TokenController } from "../lib/TokenController.js";
-import { Action, Color } from "../lib/TokenModel.js";
+import { Action, Color, Shape } from "../lib/TokenModel.js";
 
 class TokenControlStub {
   clickRedSquare() {
     this.observer.notifyThatRedSquareHasBeenClicked();
+  }
+
+  dragRedCircle() {
+    this.observer.notifyThatRedCircleHasBeenDragged();
+  }
+
+  dropOntoGreenSquare() {
+    this.observer.notifyThatGreenSquareHasBeenDroppedOnto();
   }
 
   attach(observer) {
@@ -12,12 +20,20 @@ class TokenControlStub {
 }
 
 class TokenModelStub {
-  response() {
-    return this.response_;
+  singleTokenInteraction() {
+    return this.singleTokenInteraction_;
   }
 
-  submitSingleTokenInteraction(response_) {
-    this.response_ = response_;
+  dualTokenInteraction() {
+    return this.dualTokenInteraction_;
+  }
+
+  submitSingleTokenInteraction(singleTokenInteraction_) {
+    this.singleTokenInteraction_ = singleTokenInteraction_;
+  }
+
+  submitDualTokenInteraction(dualTokenInteraction_) {
+    this.dualTokenInteraction_ = dualTokenInteraction_;
   }
 }
 
@@ -27,7 +43,21 @@ describe("Controller", () => {
     const model = new TokenModelStub();
     new TokenController(control, model);
     control.clickRedSquare();
-    expect(model.response().action).toBe(Action.touch);
-    expect(model.response().color).toBe(Color.red);
+    expect(model.singleTokenInteraction().action).toBe(Action.touch);
+    expect(model.singleTokenInteraction().token.color).toBe(Color.red);
+    expect(model.singleTokenInteraction().token.shape).toBe(Shape.square);
+  });
+
+  it("should submit use-to-touch action when user drags red circle onto green square", () => {
+    const control = new TokenControlStub();
+    const model = new TokenModelStub();
+    new TokenController(control, model);
+    control.dragRedCircle();
+    control.dropOntoGreenSquare();
+    expect(model.dualTokenInteraction().action).toBe(Action.useToTouch);
+    expect(model.dualTokenInteraction().firstToken.color).toBe(Color.red);
+    expect(model.dualTokenInteraction().firstToken.shape).toBe(Shape.circle);
+    expect(model.dualTokenInteraction().secondToken.color).toBe(Color.green);
+    expect(model.dualTokenInteraction().secondToken.shape).toBe(Shape.square);
   });
 });
