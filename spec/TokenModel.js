064e9b4292c2ffb2d9b0257a9ef943fb47758f3b
Seth Bashford <seth.bashford@boystown.org>
2021-01-28 13:18:21 -0600

    add test

---
+++ spec/TokenModel.js
@@ -0,0 +1,34 @@
+import { TokenModel, Action, Color, Shape } from "../lib/TokenModel.js";
+
+class TrialStub {
+  conclude(result) {
+    this.result_ = result;
+  }
+
+  result() {
+    return this.result_;
+  }
+}
+
+describe("Model", () => {
+  it("should submit correct trial", function () {
+    const trial = new TrialStub();
+    const model = new TokenModel(trial, [
+      {
+        token: {
+          color: Color.red,
+          shape: Shape.square,
+        },
+        action: Action.touch,
+      },
+    ]);
+    model.submitSingleTokenInteraction({
+      token: {
+        color: Color.red,
+        shape: Shape.square,
+      },
+      action: Action.touch,
+    });
+    expect(trial.result().correct).toBeTrue();
+  });
+});
