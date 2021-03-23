d7039cf91c126701f8e4efe310eaff40b8a7c5e2
Seth Bashford <seth.bashford@boystown.org>
2021-03-23 11:35:11 -0500

    add test

--- spec/TokenModel.js
+++ spec/TokenModel.js
@@ -173,6 +173,38 @@ describe("TokenModel", () => {
     );
   });
 
+  it("should not count initially correct as correct", () => {
+    testModel(
+      [
+        {
+          token: {
+            color: Color.green,
+            shape: Shape.circle,
+          },
+          action: Action.pickUp,
+        },
+      ],
+      [
+        {
+          token: {
+            color: Color.green,
+            shape: Shape.circle,
+          },
+          action: Action.pickUp,
+        },
+        {
+          token: {
+            color: Color.red,
+            shape: Shape.square,
+          },
+          action: Action.touch,
+        },
+      ],
+      false,
+      submitSingleTokenInteraction
+    );
+  });
+
   it("should not count eventually correct as correct", () => {
     testModel(
       [
