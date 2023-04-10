2b697131db1ae5719885b178408db49cb13842f7
Seth Bashford <seth.bashford@boystown.org>
2023-04-10 14:03:42 -0500

    add test

--- spec/hashing-tokens.js
+++ spec/hashing-tokens.js
@@ -7,7 +7,7 @@ import {
 } from "../lib/TokenModel.js";
 
 describe("hashing tokens", () => {
-  it("should hash each unique token to a unique value", function () {
+  it("should hash each unique sized token to a unique value", function () {
     const hashes = new Set();
     for (const size in Size) {
       for (const shape in Shape) {
@@ -26,6 +26,23 @@ describe("hashing tokens", () => {
       Object.keys(Color).length *
         Object.keys(Shape).length *
         Object.keys(Size).length
+    );
+  });
+
+  it("should hash each unique token to a unique value", function () {
+    const hashes = new Set();
+    for (const shape in Shape) {
+      for (const color in Color) {
+        hashes.add(
+          hashToken({
+            shape: Shape[shape],
+            color: Color[color],
+          })
+        );
+      }
+    }
+    expect(hashes.size).toBe(
+      Object.keys(Color).length * Object.keys(Shape).length
     );
   });
 });
